bc0420d38ce44b9e2832fbabe73f7232fd778d82
James Plank <plankjames@gmail.com>
2022-06-21 13:57:57 +0100

    Handle None case from dotenv_values

--- shocksurvey/shocksurvey/__init__.py
+++ shocksurvey/shocksurvey/__init__.py
@@ -16,7 +16,9 @@ class Env:
 
 
 try:
-    ENV = Env(**dotenv.dotenv_values(config_path))
+    env_dict = dotenv.dotenv_values(config_path)
+    env_dict = {k: (v if v is not None else "") for k, v in env_dict.items()}
+    ENV = Env(**env_dict)
 except TypeError as e:
     err = re.findall(r"'(.*?)'", str(e))[0]  # Find name of parameter
     raise NameError(f"Unable to match environment variable {err} to a parameter in class Env.\nMake sure to add the corresponding property to the Env class in shocksurvey/__init__.")  # fmt: skip
@@ -36,9 +38,9 @@ def gen_timestamp(
     if not time:
         time = dt.now()
 
-    if output_type is "pretty":
+    if output_type == "pretty":
         return f"{time:%d/%m/%Y %H:%M:%S}"
-    elif output_type is "files":
+    elif output_type == "files":
         return f"{time:%Y-%m-%dT%H-%M-%S}"
 
     return f"{time:%Y%m%d%H%M%S}"
