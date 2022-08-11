29e9608183b18240c361cc7cdc61daa8e1380571
James Plank <plankjames@gmail.com>
2022-08-11 21:11:28 +0100

    More efficient & generalised method of working with spedas files

--- shocksurvey/shocksurvey/spedas.py
+++ shocksurvey/shocksurvey/spedas.py
@@ -1,63 +1,46 @@
 from __future__ import annotations
 
-import dataclasses
-from contextlib import redirect_stdout
-from dataclasses import InitVar, asdict, dataclass
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from datetime import datetime as dt
 from datetime import timedelta
-from enum import Enum
-from io import StringIO
-from optparse import Option
-from os.path import join
-from pprint import pformat
-from typing import Any, List, Literal, Optional, Union
+from enum import Enum, auto
+from heapq import merge
+from pathlib import Path
+from typing import Any, ClassVar, NamedTuple, Type
 
 import cdflib
-import numpy as np
 import pandas as pd
+from numpy import typing as npt
 from pyspedas.mms import mms_load_fgm
 
-from shocksurvey import ENV, bprint, gen_timestamp
-from shocksurvey.mlshocks import SHK, get_plot_file
+from shocksurvey import ENV, gen_timestamp, logger
+from shocksurvey.constants import SC, Instruments
 
 
-def download_data_from_timestamp(
+def get_mms_folder_path(
     timestamp: float,
-    experiment: Literal["fgm"] = "fgm",
-    trange: Optional[List[str]] = None,
-    no_download: bool = False,
-    verbose: bool = False,
-) -> List[str]:
-    """Downloads cdf data.
-    IN:
-        timestamp:      The timestamp
-        experiment:     E.g. 'fgm' for magnetometer data
-        trange:         Set to ['YYYY-MM-DD/HH:MM:SS', 'YYYY-MM-DD/HH:MM:SS']
-                        or leave as None to generate automatically.
-        no_download:    True to get filepath (may not exist if data hasn't been downloaded)
-                        False to download data
-        verbose:        Set to True to print output from pyspedas
-    OUT:
-        fpath:          Full filepath to downloaded data
-    """
-    if not trange:
-        trange = timestamp_to_trange(timestamp)
+    sc: SC,
+    instrument: Instruments,
+    rate: str,
+    level: str,
+) -> Path:
+    t_dt = dt.utcfromtimestamp(timestamp)
+    sub_folders = Path(sc.value, instrument.value, rate, level)
+    date_folders = f"{t_dt:%Y/%m/%d}"
+    path = Path(ENV.MMS_DATA_DIR, sub_folders, date_folders)
+    path.mkdir(parents=True, exist_ok=True)
+    logger.debug(str(path))
+    return path
 
-    if experiment == "fgm":
-        with redirect_stdout(StringIO()) as stdout:
-            fpath = mms_load_fgm(trange=trange, data_rate="brst", available=True)
-            if not no_download:
-                mms_load_fgm(trange=trange, data_rate="brst", notplot=True)
-        if verbose:
-            print("\n".join(stdout))
-        return [get_mms_folder_path(timestamp, f) for f in fpath]
-    else:
-        raise NotImplementedError(f"Expreiment '{experiment}' not implemented.")
+
+def find_existing_cdf(folder_path: Path) -> list[Path]:
+    return list(folder_path.glob("*.cdf"))
 
 
 def timestamp_to_trange(
     timestamp: float, hours: int = 0, minutes: int = 1, seconds: int = 0
-) -> List[str]:
+) -> list[str]:
     t_as_dt = dt.utcfromtimestamp(timestamp)
     t_delta = t_as_dt + timedelta(hours=hours, minutes=minutes, seconds=seconds)
     trange = [
@@ -67,113 +50,137 @@ def timestamp_to_trange(
     return trange
 
 
-def get_mms_folder_path(timestamp: float, fname: str) -> str:
-    t_dt = dt.utcfromtimestamp(timestamp)
-    sub_folders = "mms1/fgm/brst/l2"
-    date_folders = f"{t_dt:%Y/%m/%d}"
-    return join(ENV.MMS_DATA_DIR, sub_folders, date_folders, fname)
-
-
-class CDF_VARS(str, Enum):
-    time = "Epoch"
-    r_time = "Epoch_state"
-    b_gse = "mms1_fgm_b_gse_brst_l2"
-    r_gse = "mms1_fgm_r_gse_brst_l2"
-    label_b_gse = "label_b_gse"
-    label_r_gse = "label_r_gse"
+def create_and_label_df(
+    data: npt.ArrayLike, time: npt.ArrayLike, labels: list[str]
+) -> pd.DataFrame:
+    col_names = [s.strip().lower().replace(" ", "_") for s in labels]
+    return pd.DataFrame(data, columns=col_names, index=pd.Series(time, name="time"))  # type: ignore
 
 
 @dataclass
-class Shock:
-    cdf_path: List[str]
-    properties: pd.Series
+class CDF(ABC):
+    instrument: ClassVar[Instruments]
+    rate: ClassVar[str]
+    level: ClassVar[str]
+    timestamp: float
+    sc: SC
+    data: dict[Enum, pd.DataFrame] | None = None
 
     def __post_init__(self):
-        self.summary_plot_path = get_plot_file(self[SHK.TIME])
-        self.data: dict[str, Union[np.ndarray, pd.DataFrame]]
-        if len(self.cdf_path) == 1:
-            self.data = self.load_data_from_cdf(self.cdf_path[0])
-        elif len(self.cdf_path) > 1:
-            self.data = self.merge_cdf()
-        else:
-            raise ValueError("At least one CDF needs to be specified")
+        self.folder: Path = get_mms_folder_path(
+            self.timestamp, self.sc, self.instrument, self.rate, self.level
+        )
+        self.trange: list[str] = timestamp_to_trange(self.timestamp)
+        self.probe = str(self.sc)[-1]
 
-    def __getitem__(self, item) -> Any:
-        if item in self.properties.index:
-            return self.properties[item]
-        else:
-            raise KeyError(f"Cannot find {item} in Shock.properties")
+    def update_trange(self, trange: list[str]) -> None:
+        self.trange = trange
 
-    def __repr__(self) -> str:
-        str = f"{' Shock ':-^80}\n"
-        str += f"{self.cdf_path = }\n"
-        str += f"{self.summary_plot_path = }\n"
-        str += f"self.properties:\n{self.properties}\n{'-'*20}\n"
-        str += f"self.data:\n{pformat(self.data)}\n{'-'*20}\n"
-        return str
+    @abstractmethod
+    def download(self) -> list[Path]:
+        pass
 
-    def shock_to_df(self) -> dict[str, Any]:
-        shock_dict = asdict(self)
-        return shock_dict
+    @abstractmethod
+    def load(self) -> None:
+        pass
 
-    def merge_cdf(self):
-        datasets = [self.load_data_from_cdf(cdf) for cdf in self.cdf_path]
-        merged = {}
-        # All datasets should have the same variables
-        keys = datasets[0].keys()
-        for k in keys:
-            data = [cdf[k] for cdf in datasets]
-            if all(isinstance(d, np.ndarray) for d in data):
-                merged[k] = np.concatenate(data, axis=0)
-            elif all(isinstance(d, pd.DataFrame) for d in data):
-                merged[k] = pd.concat(data, axis=0)  # type: ignore
-        return merged
 
-    def load_data_from_cdf(
-        self, path: str
-    ) -> dict[str, Union[np.ndarray, pd.DataFrame]]:
+class FPI(CDF):
+    instrument: ClassVar[Instruments] = Instruments.FPI
+    rate: ClassVar[str] = "brst"
+    level: ClassVar[str] = "l2"
 
+    def load(self) -> None:
+        # Check for existing data
+        cdf = find_existing_cdf(self.folder)
+        if len(cdf) == 0:
+            pass
+
+
+class FGM_LABEL(NamedTuple):
+    B: pd.DataFrame
+    R: pd.DataFrame
+
+
+class FGM(CDF):
+    instrument: ClassVar[Instruments] = Instruments.FGM
+    rate: ClassVar[str] = "brst"
+    level: ClassVar[str] = "l2"
+    labels: ClassVar[Type[FGM_LABEL]] = FGM_LABEL
+    data: FGM_LABEL | None
+
+    def get_files_list_from_server(self) -> list[Path]:
+        fpaths: list[str] = mms_load_fgm(
+            trange=self.trange,
+            probe=self.probe,
+            data_rate=self.rate,
+            available=True,
+        )
+        logger.debug("\n".join(fpaths))
+        return [Path(fp) for fp in fpaths]
+
+    def download(self) -> list[Path]:
+        fpaths: list[Path] = self.get_files_list_from_server()
+
+        mms_load_fgm(
+            trange=self.trange,
+            data_rate=self.rate,
+            notplot=True,
+            probe=self.probe,
+        )
+        return [self.folder / f for f in fpaths]
+
+    def load_cdf(self, path: Path) -> FGM_LABEL:
         cdf: cdflib.cdfread.CDF = cdflib.CDF(path)  # type: ignore
-
         info: dict[str, Any] = cdf.cdf_info()
         variables = info["zVariables"]
-        data = {}
 
-        def create_and_label_frame(var, label: str, time: np.ndarray) -> pd.DataFrame:
-            if label is None or label not in variables:
-                raise ValueError(f"Supplied label {label} not in the zVariables")
+        class CDF_VARS(str, Enum):
+            time = "Epoch"
+            r_time = "Epoch_state"
+            b_gse = f"mms{self.probe}_fgm_b_gse_brst_l2"
+            r_gse = f"mms{self.probe}_fgm_r_gse_brst_l2"
+            label_b_gse = "label_b_gse"
+            label_r_gse = "label_r_gse"
 
-            col = [s.strip() for s in cdf.varget(label)[0]]
-            var_data = cdf.varget(var)
-            return pd.DataFrame(var_data, columns=col, index=time)
+        for var in CDF_VARS:
+            assert var in variables
 
-        time: Optional[np.ndarray] = None
-        r_time: Optional[np.ndarray] = None
-        if CDF_VARS.time in variables:
-            time = cdflib.cdfepoch.unixtime(
-                cdf.varget(CDF_VARS.time), to_np=True
-            )  # type:ignore
-            data[CDF_VARS.time.name] = time
-        if CDF_VARS.r_time in variables:
-            r_time = cdflib.cdfepoch.unixtime(
-                cdf.varget(CDF_VARS.r_time), to_np=True
-            )  #  type:ignore
-            data[CDF_VARS.r_time.name] = r_time
-        if CDF_VARS.b_gse in variables:
-            assert time is not None
-            data[CDF_VARS.b_gse.name] = create_and_label_frame(
-                CDF_VARS.b_gse, CDF_VARS.label_b_gse, time
-            )
-        if CDF_VARS.r_gse in variables:
-            assert r_time is not None
-            data[CDF_VARS.r_gse.name] = create_and_label_frame(
-                CDF_VARS.r_gse, CDF_VARS.label_r_gse, r_time
-            )
+        time: npt.ArrayLike = cdflib.cdfepoch.unixtime(
+            cdf.varget(CDF_VARS.time), to_np=True
+        )
+        r_time: npt.ArrayLike = cdflib.cdfepoch.unixtime(
+            cdf.varget(CDF_VARS.r_time), to_np=True
+        )
+        b = create_and_label_df(
+            cdf.varget(CDF_VARS.b_gse),
+            time,
+            cdf.varget(CDF_VARS.label_b_gse)[0],
+        )
+        r = create_and_label_df(
+            cdf.varget(CDF_VARS.r_gse),
+            r_time,
+            cdf.varget(CDF_VARS.label_r_gse)[0],
+        )
 
-        return data
+        return FGM_LABEL(b, r)
+
+    def load(self) -> None:
+        cdf: list[Path] = [self.folder / c for c in self.get_files_list_from_server()]
+        if not all([cdf_file.exists() for cdf_file in cdf]):
+            # No files already downloaded
+            cdf = self.download()
+
+        cdf_files_data: list[FGM_LABEL] = [self.load_cdf(c) for c in cdf]
+        merged = {}
+        for i, _ in enumerate(cdf_files_data[0]):
+            data = [cdf_data[i] for cdf_data in cdf_files_data]
+            merged[i] = pd.concat(data, axis=0).sort_index()
+
+        self.data = FGM_LABEL(*[v for _, v in merged.items()])
 
 
-@dataclass
-class Data:
-    def add_var(self, var_name, var_data):
-        pass
+class FSM(CDF):
+    instrument: ClassVar[Instruments] = Instruments.FPI
+    rate: ClassVar[str] = "brst"
+    level: ClassVar[str] = "l3"
